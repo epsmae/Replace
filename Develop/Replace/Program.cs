477c6314e1f70d032a8dd938d62f6b358de948ad
Manuel Eugster <manuel.eugster@hispeed.ch>
2018-02-13 21:50:13 +0100

    Added command line parser and costura for embedding references

--- Develop/Replace/Program.cs
+++ Develop/Replace/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using CommandLine;
 
 namespace Replace
 {
@@ -6,24 +8,27 @@ namespace Replace
     {
         public static void Main(string[] args)
         {
-            if (args.Length < 3)
-            {
-                Console.WriteLine("Usage: replace.exe file.txt regex replacement");
-                return;
-            }
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(RunOptionsAndReturnExitCode)
+                .WithNotParsed<Options>(HandleParseError);
+        }
 
-            string sourceFilePath = args[0];
-            Console.WriteLine("file: " + sourceFilePath);
-            string regex = args[1];
-            Console.WriteLine("regex: " + regex);
-            string replacement = args[2];
-            Console.WriteLine("replacement: " + replacement);
+        private static void HandleParseError(IEnumerable<Error> error)
+        {
+            Console.WriteLine("Usage: replace.exe -f file -s regex -r replacement");
+        }
 
-            FileReplacer replacer = new FileReplacer(sourceFilePath);
+        private static void RunOptionsAndReturnExitCode(Options options)
+        {
+            Console.WriteLine("File: " + options.File);
+            Console.WriteLine("Search regex pattern: " + options.Regex);
+            Console.WriteLine("Replacement: " + options.Replacement);
+
+            FileReplacer replacer = new FileReplacer(options.File);
 
             try
             {
-                int count = replacer.Replace(regex, replacement);
+                int count = replacer.Replace(options.Regex, options.Replacement);
                 Console.WriteLine("Replace count: " + count);
             }
             catch (Exception ex)
--- Develop/Replace/RegexUtility.cs
+++ Develop/Replace/RegexUtility.cs
@@ -8,7 +8,7 @@ namespace Replace
         public static IList<string> GetMatches(string source, string searchRegex)
         {
             IList<string> foundMatches = new List<string>();
-            Regex filterRegex = new Regex(searchRegex, RegexOptions.CultureInvariant);
+            Regex filterRegex = new Regex(searchRegex, RegexOptions.Compiled);
             MatchCollection matches = filterRegex.Matches(source);
             
             foreach (Match match in matches)
