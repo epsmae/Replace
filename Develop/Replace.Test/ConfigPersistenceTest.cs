223c699c7f51ebe9e8e7fd3b39315c5c0069876e
Manuel Eugster <manuel.eugster@hispeed.ch>
2018-09-18 20:03:08 +0200

    Readded .net framework console application and test

---
+++ Develop/Replace.Test/ConfigPersistenceTest.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using Replace.Service;
+using Replace.Service.DataModel;
+
+namespace Replace.Test
+{
+    [TestFixture]
+    public class ConfigPersistenceTest : Testbase
+    {
+        private string TestDataPath
+        {
+            get { return Path.Combine(TestSource, "config.xml"); }
+        }
+
+        private ConfigPersistence _persistence;
+
+        [SetUp]
+        public void Setup()
+        {
+            _persistence = new ConfigPersistence();            
+        }
+
+        [Test]
+        public void TestSerializeDeserialize()
+        {
+            Config config = new Config()
+            {
+                RegexReplaceValues = new List<RegexReplaceValue>()
+                {
+                    new RegexReplaceValue{Regex = "Regex1", ReplaceValue = "Value1"},
+                    new RegexReplaceValue{Regex = "Regex2", ReplaceValue = "Value2"},
+                    new RegexReplaceValue{Regex = "Regex3", ReplaceValue = "Value3"}
+                },
+                FileExtensions = new List<string>()
+                {
+                    ".png", ".txt"
+                },
+                PathToSearch = "..\\..\\Develop"
+            };
+            
+            _persistence.Save(TestDataPath, config);
+            Config loadedConfig = _persistence.Load(TestDataPath);
+            AssertAreEqual(config, loadedConfig);
+
+        }
+
+        private void AssertAreEqual(Config expected, Config actual)
+        {
+            AssertAreEqual(expected.RegexReplaceValues, actual.RegexReplaceValues);
+            AssertAreEqual(expected.FileExtensions, actual.FileExtensions);
+            Assert.AreEqual(expected.PathToSearch, actual.PathToSearch);
+        }
+
+        private void AssertAreEqual(List<string> expected, List<string> actual)
+        {
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        private void AssertAreEqual(List<RegexReplaceValue> expected, List<RegexReplaceValue> actual)
+        {
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i].Regex, actual[i].Regex);
+                Assert.AreEqual(expected[i].ReplaceValue, actual[i].ReplaceValue);
+            }
+        }
+    }
+}
---
+++ Develop/Replace/Options.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+
+namespace Replace.App
+{
+    internal class Options
+    {
+        [Option('s', "search", Required = false, HelpText = "Search regex pattern")]
+        public string Regex { get; set; }
+
+        [Option('f', "file", Required = false, HelpText = "File to search and replace")]
+        public string File { get; set; }
+
+        [Option('r', "replacement", Required = false, HelpText = "Replacement text")]
+        public string Replacement { get; set; }
+
+        [Option('c', "Config", Required = false, HelpText = "Config file path")]
+        public string Config { get; set; }
+
+        [Option('t', "tagreplacements", Required = false, HelpText = "Tags which will be replaced by the value")]
+        public IList<string> TagReplacements { get; set; }
+    }
+}
